abd3871e841f92118d47cd6ed287c36b72a4f475
trevorantram <trevor2076@gmail.com>
2016-06-02 19:52:54 -0400

    Add files via upload
    added some files

---
+++ src/Skills/Read.java
@@ -0,0 +1,12 @@
+package com.company.Skills;
+
+/**
+ * Package com.company.Skills
+ * Created by trevor2076 on 6/2/16/Jun/2016}.
+ * <p>
+ * The following code may not be used without the explicit permission of trevor2076
+ */
+public class Read {
+
+
+}
