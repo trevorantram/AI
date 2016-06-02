81121575a59389957578ab521f595a2b0fbb11de
trevorantram <trevor2076@Shauns-MBP.home>
2016-06-02 19:49:15 -0400

    second commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Package PACKAGE_NAME
+ * Created by trevor2076 on 6/2/16/Jun/2016}.
+ * <p>
+ * The following code may not be used without the explicit permission of trevor2076
+ */
+public class Main {
+}
